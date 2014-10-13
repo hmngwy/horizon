52841d86b886d724a5f0420fb4031e034318cebf
hmngwy <cp.ambrosio@gmail.com>
2014-10-13 15:30:25 +0800

    mongolab uri

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/sesh', function(req, res) {
 });
 
 // MONGOOSE SCHEMA SETUP
-mongoose.connect("mongodb://localhost/tree");
+mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/tree");
 
 var PostSchema = mongoose.Schema({
     body: { type: String, index: false },
