632a89b29135ea969c3b50acaf2230a095965066
hmngwy <cp.ambrosio@gmail.com>
2014-10-15 20:03:34 +0800

    otp validation for pruning

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var express             = require('express'),
 var PUBLIC_KEY  = process.env.RECAPTCHA_KEY || '6LdS2_sSAAAAAMLZbnArIERAhwutv0KaQIP6lAWs',
     PRIVATE_KEY = process.env.RECAPTCHA_KEY_PRIVATE || '6LdS2_sSAAAAAAOgKurCsZRyJ2RPhKBf1alCxwRn';
 
+
+
 // EXPRESS SETUP
 var app = express();
 app.use(bodyParser());
@@ -29,7 +31,7 @@ app.get('/sesh', function(req, res) {
 });
 
 // MONGOOSE SCHEMA SETUP
-mongoose.connect( (process.env.NODE_ENV=='development') ? "mongodb://localhost/tree3" : process.env.MONGOLAB_URI );
+mongoose.connect( (process.env.NODE_ENV=='development') ? "mongodb://localhost/tree" : process.env.MONGOLAB_URI );
 
 var PostSchema = mongoose.Schema({
     body: { type: String, index: false },
@@ -38,7 +40,6 @@ var PostSchema = mongoose.Schema({
     meta: { type: Object, index: false },
     responses: {type:Number, default: 0}
 });
-
 PostSchema.pre('save', function(next){
 
     if(this.parentId!=undefined)
@@ -48,7 +49,6 @@ PostSchema.pre('save', function(next){
     next();
 });
 PostSchema.post('save', function(doc){
-
     if(doc.parentId)
         Post.count({parentId:doc.parentId}, function(err, count){ //get nodes under this parentID
             Post.findOne({_id:doc.parentId}, function(err, post){ //get the parent node
@@ -59,7 +59,6 @@ PostSchema.post('save', function(doc){
                 }
             });
         });
-
 });
 
 PostSchema.plugin(materializedPlugin);
@@ -81,6 +80,67 @@ Resource.route('immediate', {
         Post.find({parentId:req.params.id}).lean().exec(function(err, docs){
             res.send(JSON.stringify(docs));
         });
+    }
+});
+Resource.route('evaporate', {
+    detail: true,
+    handler: function(req, res, next){
+
+        var b32                 = require('thirty-two'),
+            jsSHA               = require('jssha');
+        var otp_key = b32.encode('saaaa').toString();
+
+        function dec2hex(s) { return (s < 15.5 ? '0' : '') + Math.round(s).toString(16); }
+        function hex2dec(s) { return parseInt(s, 16); }
+
+        function base32tohex(base32) {
+            var base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+            var bits = "";
+            var hex = "";
+
+            for (var i = 0; i < base32.length; i++) {
+                var val = base32chars.indexOf(base32.charAt(i).toUpperCase());
+                bits += leftpad(val.toString(2), 5, '0');
+            }
+
+            for (var i = 0; i+4 <= bits.length; i+=4) {
+                var chunk = bits.substr(i, 4);
+                hex = hex + parseInt(chunk, 2).toString(16) ;
+            }
+            return hex;
+
+        }
+
+        function leftpad(str, len, pad) {
+            if (len + 1 >= str.length) {
+                str = Array(len + 1 - str.length).join(pad) + str;
+            }
+            return str;
+        }
+
+        function updateOtp(inputkey, decay) {
+
+            var key = base32tohex(inputkey);
+            var epoch = Math.round((new Date().getTime()+ (decay*1000)) / 1000.0);
+            var time = leftpad(dec2hex(Math.floor(epoch / 30)), 16, '0');
+
+            var hmacObj = new jsSHA(time, 'HEX');
+            var hmac = hmacObj.getHMAC(key, 'HEX', 'SHA-1', "HEX");
+
+            if (hmac != 'KEY MUST BE IN BYTE INCREMENTS') {
+                var offset = hex2dec(hmac.substring(hmac.length - 1));
+                var part1 = hmac.substr(0, offset * 2);
+                var part2 = hmac.substr(offset * 2, 8);
+                var part3 = hmac.substr(offset * 2 + 8, hmac.length - offset);
+            }
+
+            var otp = (hex2dec(hmac.substr(offset * 2, 8)) & hex2dec('7fffffff')) + '';
+            otp = (otp).substr(otp.length - 6, 6);
+
+            return {otp:otp, hmac:hmac};
+        }
+        res.send(updateOtp(otp_key, 0).otp + ' : ' + updateOtp(otp_key, -30).otp + ' : ' + updateOtp(otp_key, 30).otp);
+        //next();
     }
 });
 Resource.route('tree', {
@@ -123,7 +183,6 @@ Resource.route('ancestors', {
         });
     }
 });
-
 Resource.before('post', function(req, res, next){
 
     delete req.body.user;
@@ -175,4 +234,4 @@ Resource.before('post', function(req, res, next){
 
 // RUN APP
 Resource.register(app, '/n');
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
